f22fffa06c76af162e76ad67cc6d7eacbdc0339a
felipenrocha19 <felipenrocha19@gmail.com>
2023-01-24 23:53:18 -0300

    Made module that encrypt and decrypt messages generated by keys made by rsa algorithm

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 
 
 from src.key_gen import KeyGen
+from src.encryption import encrypt, decrypt
+
+
 import time
 
 def main():
-    key_gen = KeyGen()
+    
 
-    return 0
+    bob = KeyGen()
+    m = 'Hello World!'
+
+
+    public_key = bob.get_public_key()
+    private_key = bob.get_private_key()
+
+    print('Public Key:', public_key)
+    print('Private Key:', private_key)
+    print('Original Message:', m)
+    c = encrypt(m, public_key)
+    print('Criptogram:', c)
+    m = decrypt(c, private_key)
+    print('Message Decrypted', m)
+
 
 
 if __name__ == '__main__':
